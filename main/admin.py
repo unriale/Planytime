a63db40a16183c0c54bcc3c12c6932bfbabdc669
Kateryna Kvasniak <kateryna.kvasniak@gmail.com>
2022-05-18 06:44:21 +0200

    added Event model

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import Note
+from .models import Note, Event
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'date')
+    list_filter = ('user', 'date')
+
 
 admin.site.register(Note)
+admin.site.register(Event, EventAdmin)
--- main/models.py
+++ main/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
+
 
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     body = models.TextField()
+
+
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.TextField(max_length=100)
+    date = models.DateField(default=datetime.date.today)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    colorTypeId = models.IntegerField()
+    dayIndex = models.IntegerField()
